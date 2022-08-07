c8d152055719ce4449825625eb02a1f6ca057fb0
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-08-07 15:59:34 -0600

    feat: add plus 2 home button

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let homeScore = document.getElementById("home-score")
 let updatedHomeCount = 0
+
 function addOneHomeScore() {
   updatedHomeCount += 1
-  homeScore.innerText = updatedHomeCount
+  homeScore.textContent = updatedHomeCount
+}
 
+function addTwoHomeScore() {
+  updatedHomeCount += 2
+  homeScore.textContent = updatedHomeCount
 }
